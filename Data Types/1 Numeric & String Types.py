8dfbda801801025affbc4feac71df22dceea0478
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-07-22 01:21:46 +0600

    first commit

---
+++ Data Types/1 Numeric & String Types.py
@@ -0,0 +1,22 @@
+#Dynamic Data Type
+age=20 #int
+price=699.89 #float
+x=1+2j #complex
+
+name= 'Maruf' # single quote string
+city= "Dhaka" # double quote string
+address=''' 
+Mirpur,
+Dhaka,
+Bangladesh
+'''  #triple single quote string
+
+university=""" 
+Bangladesh university
+of
+Business & 
+Technology 
+"""
+
+
+print(university)
